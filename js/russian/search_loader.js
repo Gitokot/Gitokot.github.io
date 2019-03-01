c31cbb6185f2ec34fa7354934e13993947ba11ff
Gitokot <42905192+Gitokot@users.noreply.github.com>
2019-03-01 23:03:43 +0300

    Add files via upload
    css

--- js/russian/search_loader.js
+++ js/russian/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2019-03-01T22:48:38+03:00';
+        s.src = '/js/' + language + '/search.js?2019-03-01T23:02:20+03:00';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
